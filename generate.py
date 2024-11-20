1fc0a6e9b3c0f09abd0f1dd0b584d58304c2c6dd
Maciej Olko <maciej.olko@gmail.com>
2024-11-20 01:42:40 +0100

    Format with black

--- generate.py
+++ generate.py
@@ -29,13 +29,21 @@ with TemporaryDirectory() as clones_dir:
         clone_path = Path(clones_dir, language)
         for branch in ('3.13', '3.12', '3.11', '3.10', '3.9'):
             try:
-                Repo.clone_from(f'https://github.com/python/python-docs-{language}.git', clone_path, depth=1, branch=branch)
+                Repo.clone_from(
+                    f'https://github.com/python/python-docs-{language}.git', clone_path, depth=1, branch=branch
+                )
             except GitCommandError:
                 print(f'failed to clone {language} {branch}')
                 continue
             try:
                 with TemporaryDirectory() as tmpdir:
-                    completion = merge_and_scan_path(clone_path, pot_path=Path(clones_dir, 'cpython/Doc/build/gettext'), tmpdir=Path(tmpdir), hide_reserved=False, api_url='').completion
+                    completion = merge_and_scan_path(
+                        clone_path,
+                        pot_path=Path(clones_dir, 'cpython/Doc/build/gettext'),
+                        tmpdir=Path(tmpdir),
+                        hide_reserved=False,
+                        api_url='',
+                    ).completion
             except OSError as e:
                 print(f'failed to scan {language} {branch}: {e}')
                 rmtree(clone_path)
@@ -45,33 +53,33 @@ with TemporaryDirectory() as clones_dir:
         completion_progress.append((language, completion))
         print(completion_progress[-1])
 
-template = Template("""
-<html lang="en">
-<head>
-<title>Python Docs Translation Dashboard</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<h1>Python Docs Translation Dashboard</h1>
-<table>
-<thead>
-<tr><th>language</th><th>completion</th></tr>
-</thead>
-<tbody>
-{% for language, completion in completion_progress | sort(attribute=1) | reverse %}
-<tr>
-  <td data-label="language">{{ language }}</td>
-  <td data-label="completion">
-    <div class="progress-bar" style="width: {{ completion | round(2) }}%;">{{ completion | round(2) }}%</div>
-  </td>
-</tr>
-{% endfor %}
-</tbody>
-</table>
-<p>Last updated at {{ generation_time.strftime('%A, %d %B %Y, %X %Z') }}.</p>
-</body>
-</html>
-""")
+template = Template(
+    """<html lang="en">
+    <head>
+      <title>Python Docs Translation Dashboard</title>
+      <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h1>Python Docs Translation Dashboard</h1>
+    <table>
+    <thead>
+    <tr><th>language</th><th>completion</th></tr>
+    </thead>
+    <tbody>
+    {% for language, completion in completion_progress | sort(attribute=1) | reverse %}
+    <tr>
+      <td data-label="language">{{ language }}</td>
+      <td data-label="completion">
+        <div class="progress-bar" style="width: {{ completion | round(2) }}%;">{{ completion | round(2) }}%</div>
+      </td>
+    </tr>
+    {% endfor %}
+    </tbody>
+    </table>
+    <p>Last updated at {{ generation_time.strftime('%A, %d %B %Y, %X %Z') }}.</p>
+    </body>
+    </html>"""
+)
 
 output = template.render(completion_progress=completion_progress, generation_time=generation_time)
 
